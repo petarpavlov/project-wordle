48ae1c9afa779e3673b165e2219f41f554e9e55f
petarpavlov <contact@petarpavlov.com>
2024-08-24 10:04:33 +0200

    GuessResults 1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import GuessInput from '../GuessInput/GuessInput';
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -12,6 +13,7 @@ console.info({ answer });
 function Game() {
   return (
     <>
+      <GuessResults />
       <GuessInput />
     </>
   );
---
+++ src/components/GuessResults/GuessResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GuessResults() {
+  return (
+    <div className="guess-results">
+      <ul>
+        <li className="guess">FIRST</li>
+        <li className="guess">GUESS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default GuessResults;
